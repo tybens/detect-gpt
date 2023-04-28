e81014227e0960f2a00ac8bc96c7a08389fb92f1
tybens <tb19@princeton.edu>
2023-04-28 15:09:36 -0400

    update output name

--- paper_scripts/cos484/bart-base.sh
+++ paper_scripts/cos484/bart-base.sh
@@ -1,7 +1,7 @@
 
 # bert-large-uncased
-python run.py --output_name main --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.1 --span_length 2
-python run.py --output_name main --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-python run.py --output_name main --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.5 --span_length 2
-python run.py --output_name main --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.6 --span_length 2
-python run.py --output_name main --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.8 --span_length 2
+python run.py --output_name cos484-bart-base --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.1 --span_length 2
+python run.py --output_name cos484-bart-base --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
+python run.py --output_name cos484-bart-base --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.5 --span_length 2
+python run.py --output_name cos484-bart-base --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.6 --span_length 2
+python run.py --output_name cos484-bart-base --base_model_name gpt2 --mask_filling_model_name facebook/bart-base --n_perturbation_list 100 --n_samples 500 --pct_words_masked 0.8 --span_length 2
